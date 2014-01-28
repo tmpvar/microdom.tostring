5e2377932941a56d7992b39e9d56ba32b0f147c2
Elijah Insua <tmpvar@gmail.com>
2014-01-28 15:19:03 -0700

    first round stringification of a microdom

--- tostring.js
+++ tostring.js
@@ -1,16 +1,40 @@
-var tostring = function() {
-  
+if (typeof require !== 'undefined') {
+  var microdom = require('microdom');
 }
 
+microdom.plugin({
+  toString : function(level) {
+    if (this.name === 'text') {
+      return this.value;
+    }
 
-if (typeof module !== "undefined" && typeof module.exports == "object") {
-  module.exports = tostring;
-}
+    var ret = '';
+    level = level || 0;
+    if (this.name) {
+      
+      ret += '<' + this.name;
 
-if (typeof window !== "undefined") {
-  window.tostring = window.tostring || tostring;
-}
+      Object.keys(this.attributes).forEach(function(attr) {
+        ret += ' ' + attr + '="' + this.attr(attr) + '"';
+      }.bind(this));
+    }
 
-module.exports = function() {
+    if (this.length()) {
+      if (this.name) {
+        ret += ">";
+      }
 
-}
+      for (var i = 0; i<this.length(); i++) {
+        ret += this.child(i).toString(level + 1);
+      }
+
+      if (this.name) {
+        ret += '</' + this.name + '>\n';
+      }
+    } else {
+      ret += " />";
+    }
+
+    return ret;
+  }
+});
